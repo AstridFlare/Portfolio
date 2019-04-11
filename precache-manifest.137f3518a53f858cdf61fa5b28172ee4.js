88d5351ef5efca63515a74312d9bc5085d4a5e37
AstridFlare <AstridFlare@Gmail.com>
2019-04-10 23:44:27 -0700

    Updates

---
+++ precache-manifest.137f3518a53f858cdf61fa5b28172ee4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b444b93356014ea8229f",
+    "url": "/portfolio/static/js/main.b444b933.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b444b93356014ea8229f",
+    "url": "/portfolio/static/css/main.0dfa95c9.chunk.css"
+  },
+  {
+    "revision": "373fb395e74db9c34fb8e7cb11a429a3",
+    "url": "/portfolio/index.html"
+  }
+];
